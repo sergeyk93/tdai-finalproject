62c7bea2427d43045bf2c9540e07ef17b6390e49
sergeyk93 <sergeyk93@gmail.com>
2016-03-10 12:55:07 +0200

    test

--- src/ai/WaveGenerator.java
+++ src/ai/WaveGenerator.java
@@ -12,7 +12,7 @@ public class WaveGenerator {
 	public WaveGenerator(){
 		pc = new PriceCalculator();
 	}
-	
+	//test
 	public ArrayList<Creature> generate() throws Exception{
 		if (Jeu.getNumVagueCourante() / 10 > Jeu.getUpgrade()){
 			Jeu.incUpgrade();
